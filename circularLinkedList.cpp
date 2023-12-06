c825a1fe842fdb8e4e41f9bb820765c9ca021a80
itsmeankeet <adheekariankit007@gmail.com>
2023-12-06 18:58:08 +0545

    Class Added

--- circularLinkedList.cpp
+++ circularLinkedList.cpp
@@ -1,8 +1,25 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 struct Node
 {
- int data;
- Node *next;   
+    int data;
+    Node *next;
 };
+
+class circularLinkedList
+{
+private:
+    Node *start;
+public:
+    circularLinkedList(/* args */);
+    ~circularLinkedList();
+};
+
+circularLinkedList::circularLinkedList(/* args */)
+{
+}
+
+circularLinkedList::~circularLinkedList()
+{
+}
