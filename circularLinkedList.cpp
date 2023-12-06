60ad63a6730ca5240f9ab96c0e0e42c78704a254
itsmeankeet <adheekariankit007@gmail.com>
2023-12-06 19:47:22 +0545

    Circular Git Problem Doesnt Solve

--- circularLinkedList.cpp
+++ circularLinkedList.cpp
@@ -17,6 +17,58 @@ public:
     Node *createNode(int data)
     {
         Node *newNode = new Node();
+        newNode->data = data;
+        if(!start)
+        {
+            start = newNode;
+            newNode->next = start;
+        }
+        return newNode;
+    }
+
+    void insertFromFront(int data)
+    {
+        Node *newNode = createNode(data);
+        if(!newNode)
+        {
+            cout<<"Error in insertFromFront:: Cannot create newnode \n";
+        }
+        newNode->next = start;
+        start->next = newNode;
+        start = newNode;
+    }
+
+    void deleteFront()
+    {
+        Node *temp = start;
+        if(!temp)
+        {
+            cout<<"Error in deleteFront::start is null \n";
+        }
+        Node *previous = temp;
+        while(temp->next!=start)
+        {
+            previous = temp;
+            temp = temp->next;
+        }
+        previous->next = start;
+        delete temp;
+    }
+
+
+    void display()
+    {
+        Node *temp = start;
+        if(!temp)
+        {
+            cout<<"Node is empty \n";
+        }
+        while(temp->next != start)
+        {
+            cout<<temp->data<<"->";
+            temp= temp->next;
+        }
+        cout<<"circular \n";
     }
 };
 
@@ -27,4 +79,22 @@ circularLinkedList::circularLinkedList(/* args */)
 
 circularLinkedList::~circularLinkedList()
 {
+}
+
+
+int main()
+{
+    circularLinkedList ob;
+    cout<<"Creating Node \n";
+    ob.createNode(10);
+    ob.display();
+    cout<<"Inserting in front \n";
+    ob.insertFromFront(20);
+    ob.display();
+    cout<<"Inserting in front \n";
+    ob.insertFromFront(30);
+    ob.display();
+    cout<<"Deleting front \n";
+    ob.deleteFront();
+    ob.display();
 }
